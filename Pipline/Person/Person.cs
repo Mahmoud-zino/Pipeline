3d676351b564787dacb26633b99b3e2862681d4b
Mahmoud-zino <mahmoud.zino@liebherr.com>
2021-06-16 11:00:58 +0200

    Initiate Person library

---
+++ Pipline/Person/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ElZino.Models.PersonLib
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        public Person() { }
+        public Person(string firstname, string lastname)
+        {
+            this.FirstName = firstname;
+            this.LasttName = lastname;
+        }
+        public string FirstName
+        {
+            get => this.firstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception($"{nameof(Person)}:{nameof(FirstName)}");
+                this.firstName = value;
+            }
+        }
+        public string LasttName
+        {
+            get => this.lastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception($"{nameof(Person)}:{nameof(LasttName)}");
+                this.lastName = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LasttName}";
+        }
+    }
+}
